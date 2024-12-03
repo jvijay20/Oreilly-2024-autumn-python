724dc5b1b8d51d847398cb75caa232b1fed10c12
Reuven Lerner <reuven@lerner.co.il>
2024-12-03 21:28:12 +0200

    gitautosync update 73

--- menu.py
+++ menu.py
@@ -2,3 +2,12 @@ x = 123
 
 def hello(name):
     return f'Hello, {name} from menu'
+
+def menu(options):
+    while True:
+        choice = input('Enter your choice: ').strip()
+
+        if choice in options:
+            return choice
+
+        print(f'Bad option; try again')
